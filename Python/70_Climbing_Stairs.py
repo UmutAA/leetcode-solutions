317ac5f6af28609cb1d9faec444ade956f0e4e4a
UmutAA <201361309+UmutAA@users.noreply.github.com>
2025-09-21 08:21:19 +0300

    Solve: Climbing Stairs | Python O(n) / Fixed Latest Commit

--- Python/70_Climbing_Stairs.py
+++ Python/70_Climbing_Stairs.py
@@ -1,6 +1,14 @@
 class Solution:
+    def __init__(self):
+        self.memo = {}   # storage for already computed results
+
     def climbStairs(self, n: int) -> int:
         if n <= 2:
             return n
-        else:
-            return (self.climbStairs(n - 1) + self.climbStairs(n - 2))
+
+        if n in self.memo:          # if already computed, just reuse it
+            return self.memo[n]
+
+        # otherwise compute and store it
+        self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.memo[n]
