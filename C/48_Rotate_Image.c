5c2ab874411002f2043cec3e1e13dfb54a9ec6f3
UmutAA <201361309+UmutAA@users.noreply.github.com>
2025-10-13 12:35:31 +0300

    48_Rotate_Image solved

---
+++ C/48_Rotate_Image.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void rotate(int** matrix, int matrixSize, int* matrixColSize) {
+    // Transpose
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = i + 1; j < matrixSize; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    // Reverse each row
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize / 2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][matrixSize - j - 1];
+            matrix[i][matrixSize - j - 1] = temp;
+        }
+    }
+}
