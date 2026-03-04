e2bfe9d64f65049ef7bca0f716d23e906bc2bfc3
UmutAA <201361309+UmutAA@users.noreply.github.com>
2026-03-04 14:56:07 +0300

    26 - Optimization for 26 - Remove Duplicates from Sorted Array

--- Java/Remove_Duplicates_from_Sorted_Array.java
+++ Java/Remove_Duplicates_from_Sorted_Array.java
@@ -1,7 +1,6 @@
 public class Remove_Duplicates_from_Sorted_Array {
     int removeDuplicates(int[] nums)
     {
-        int k = 1;
         int length = nums.length;
         int i = 0, j = 1;
         while (j < length)
@@ -10,10 +9,9 @@ public class Remove_Duplicates_from_Sorted_Array {
             {
                 i++;
                 nums[i] = nums[j];
-                k++;
             }
             j++;
         }
-        return k;
+        return i + 1;
     }
 }
