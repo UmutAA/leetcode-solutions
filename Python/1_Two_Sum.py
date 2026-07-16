6b9b853bd579019de23c73f24f3b6ca3b3d783b3
UmutAA <201361309+UmutAA@users.noreply.github.com>
2026-07-16 10:00:37 +0300

    Updated 1_Two_Sum.py

--- Python/1_Two_Sum.py
+++ Python/1_Two_Sum.py
@@ -2,10 +2,9 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[num] = i
+        indices = dict()
+        for i,num in enumerate(nums):
+            diff = target - num
+            if diff in indices and indices[diff] != i:
+                return [indices[num], i]
         return []
