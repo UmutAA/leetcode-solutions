ab8cc4f2bc504eff8e6c292c472f6a6c021ae260
UmutAA <201361309+UmutAA@users.noreply.github.com>
2026-07-16 13:58:00 +0300

    Update 125 Valid Palindrome in Python

--- Python/125_Valid_Palindrome.py
+++ Python/125_Valid_Palindrome.py
@@ -1,8 +1,21 @@
 class Solution:
+    #This solution works fine, yet it works with O(n) time and extra space complexity
+    """
     def isPalindrome(self, s: str) -> bool:
-        cleaned = ''.join(ch for ch in s if ch.isalnum())
-        cleaned = cleaned.lower()
-        if cleaned == cleaned[::-1]:
-            return True
-        else:
-            return False
+        filtered_str = "".join([char for char in s if char.isalphanum()])
+        filtered = filtered_str.lower()
+        return filtered_str == filtered_str[::-1]
+    """
+    #This one works also fine, plus it works with O(n) time and O(1) extra space complexity
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
