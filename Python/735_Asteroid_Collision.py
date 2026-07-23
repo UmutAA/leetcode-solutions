f1ad3b554cedbe333cd07853f1e78f68c6662470
UmutAA <201361309+UmutAA@users.noreply.github.com>
2026-07-23 14:18:43 +0300

    735 Asteroid Collision in Python

---
+++ Python/735_Asteroid_Collision.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for ast in asteroids:
+            while stack and ast < 0 and stack[-1] > 0:
+                diff = ast + stack[-1]
+                if diff < 0:
+                    stack.pop()
+                elif diff == 0:
+                    stack.pop()
+                    ast = 0
+                else:
+                    ast = 0
+            if ast != 0:
+                stack.append(ast)
+
+        return stack
+                
